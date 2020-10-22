9533acc2d17ecf69e97c8006bab111c8b94b3cec
Tim <xyshen@umich.edu>
2020-10-22 15:42:15 -0500

    update gdb activities

--- gdb/act2.c
+++ gdb/act2.c
@@ -4,7 +4,7 @@ int stc(char*, char*);
 
 int main(int argc, char** argv)
 {
-    int ret = stc("cs429h", argv[argc-1]);
+    int ret = stc("cs429", argv[argc-1]);
     argv[0] = '\0';  // NOOP to force gcc to generate a callq instead of jmp
     return ret;
 }
